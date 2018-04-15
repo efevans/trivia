d1504620a73bf64abef3c7897c3a00cacee44352
efevans <efevans1@gmail.com>
2018-04-15 16:48:52 -0700

    Fill out the GetQuestions implementation for JServiceQuestionGetter

--- jservicequestiongetter.go
+++ jservicequestiongetter.go
@@ -1,11 +1,43 @@
 package trivia
 
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+)
+
 // JServiceQuestionGetter implements the QuestionGetter interface using the jservice api
 type JServiceQuestionGetter struct {
-	count int
+	questionCount int
 }
 
 // GetQuestions gets a collection of questions from the jservice api
-func (*JServiceQuestionGetter) GetQuestions() []Question {
-	return nil
+func (getter *JServiceQuestionGetter) GetQuestions() []Question {
+	count := getter.getCount()
+	resp, err := http.Get("http://jservice.io/api/random?count=" + strconv.Itoa(count))
+	var questions = &[]Question{}
+
+	if err == nil {
+		defer resp.Body.Close()
+		contents, err := ioutil.ReadAll(resp.Body)
+		if err == nil {
+			bad := json.Unmarshal(contents, questions)
+			if bad != nil {
+				return nil
+			}
+		} else {
+			return nil
+		}
+	} else {
+		return nil
+	}
+	return *questions
+}
+
+func (getter *JServiceQuestionGetter) getCount() int {
+	if getter.questionCount > 1 || getter.questionCount > 30 {
+		return getter.questionCount
+	}
+	return 10
 }
